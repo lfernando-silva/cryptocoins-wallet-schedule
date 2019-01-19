008b1b67a4b0446f8d2f158f2bf45f1cdcd1ec68
Luiz Fernando <lfernando-silva@hotmail.com>
2019-01-19 11:58:09 -0200

    Pagination moves

--- js/elements/injections.js
+++ js/elements/injections.js
@@ -1,4 +1,6 @@
-const walletsListTableTbody = document.getElementById('wallet-list-table-tbody');
+ walletsListTableTbody = document.getElementById('wallet-list-table-tbody');
+const paginationUl = document.getElementById('pagination-ul');
+let numberOfPages = null;
 
 const injectToolTip = (element, title) => {
     element['data-toggle'] = 'tooltip';
@@ -16,14 +18,30 @@ const injectTableRole = ({tr, th, td1, td2, td3}) => {
 };
 
 const injectWallets = wallets => {
-    wallets.forEach((w, index) => {
+
+    if (!wallets || wallets.length === 0) {
+        const trNone = document.createElement('tr');
+        const tdNone = document.createElement('td');
+        trNone.id = 'none-contact';
+        tdNone.colSpan = '5';
+        tdNone.innerText = 'No contact yet. Click on \"+\" to add one!';
+        trNone.appendChild(tdNone);
+        walletsListTableTbody.appendChild(trNone);
+        return true;
+    }
+
+    while (walletsListTableTbody.firstChild) {
+        walletsListTableTbody.removeChild(walletsListTableTbody.firstChild);
+    }
+
+    wallets.forEach(w => {
         const {tr, th, td1, td2, td3} = createTableRole();
 
         tr.id = w.id;
         injectToolTip(tr, `Click to Copy ${w.label} Address`);
 
         th.scope = "row";
-        th.innerText = index + 1;
+        th.innerText = w.id;
 
         td1.innerText = w.label;
         td2.innerText = w.cryptocurrency;
@@ -51,3 +69,84 @@ const injectWallets = wallets => {
     });
 };
 
+const getExtremityPaginateButton = (ariaLabel, innerText) => {
+    const li = document.createElement('li');
+    const a = document.createElement('a');
+    const spanAriaHidden = document.createElement('span');
+    const spanSrOnly = document.createElement('span');
+
+    li.className = 'page-item';
+    a.className = 'page-link';
+    a.href = '#';
+    a.setAttribute('aria-label', ariaLabel);
+    spanAriaHidden.setAttribute('aria-hidden', true);
+    spanAriaHidden.innerText = innerText;
+    spanSrOnly.className = 'sr-only';
+    spanSrOnly.innerText = ariaLabel;
+
+    a.appendChild(spanAriaHidden);
+    a.appendChild(spanSrOnly);
+    li.appendChild(a);
+
+    return li;
+};
+
+const previous = getExtremityPaginateButton('Previous', '<<');
+const next = getExtremityPaginateButton('Next', '>>');
+
+const injectPagination = wallets => {
+    numberOfPages = Object.keys(wallets).length;
+
+    const pageViewed = Number(localStorage.getItem('pageViewed'));
+
+    if (pageViewed === 1) {
+        previous.className = `${previous.className} disabled`;
+    }
+
+    if(pageViewed === numberOfPages){
+        next.className = `${next.className} disabled`;
+    }
+
+    previous.addEventListener('click', () => {
+        const pageViewed = Number(localStorage.getItem('pageViewed'));
+        if (pageViewed === 1) {
+            previous.className = `${previous.className} disabled`;
+            next.className = `${next.className}`.replace('disabled', '');
+        } else {
+            const page = pageViewed - 1;
+            previous.className = `${previous.className}`.replace('disabled', '');
+            next.className = `${next.className}`.replace('disabled', '');
+            localStorage.setItem('pageViewed', page);
+
+            if(page === 1){
+                previous.className = `${previous.className} disabled`;
+                next.className = `${next.className}`.replace('disabled', '');
+            }
+
+            injectWallets(wallets[page]);
+        }
+    });
+
+    next.addEventListener('click', () => {
+        const pageViewed = Number(localStorage.getItem('pageViewed'));
+        if(pageViewed === numberOfPages){
+            next.className = `${next.className} disabled`;
+            previous.className = `${previous.className}`.replace('disabled', '');
+        } else {
+            const page = pageViewed + 1;
+            previous.className = `${previous.className}`.replace('disabled', '');
+            next.className = `${next.className}`.replace('disabled', '');
+            localStorage.setItem('pageViewed', page);
+
+            if(page === numberOfPages){
+                previous.className = `${previous.className}`.replace('disabled', '');
+                next.className = `${next.className} disabled`;
+            }
+
+            injectWallets(wallets[page]);
+        }
+    });
+
+    paginationUl.appendChild(previous);
+    paginationUl.appendChild(next);
+};
--- js/popup/index.js
+++ js/popup/index.js
@@ -1,34 +1,112 @@
-const wallets = [
-    {
-        id: 1,
-        label: "Lunes Wallet",
-        cryptocurrency: "Lunes",
-        address: "598jrejoiewiojfepfepmqwp932iuj",
-        timestamp: new Date().getTime()
-    },
-    {
-        id: 2,
-        label: "Lunes NEWC",
-        cryptocurrency: "Lunes",
-        address: "598crejoiewiojfepfepmqwp932iuj",
-        timestamp: new Date().getTime()
-    },
-    {
-        id: 3,
-        label: "ETH Chrome",
-        cryptocurrency: "Ethereum",
-        address: "598jrejoiewiojfepfvpmqwp932iuj",
-        timestamp: new Date().getTime()
-    },
-    {
-        id: 4,
-        label: "Freebitcoin",
-        cryptocurrency: "Bitcoin",
-        address: "698jrejoiewiojfepfepmqwp932ixj",
-        timestamp: new Date().getTime()
-    },
-];
+const wallets = {
+    1: [
+        {
+            id: 1,
+            label: "Lunes Wallet",
+            cryptocurrency: "Lunes",
+            address: "598jrejoiewiojfepfepmqwp932iuj",
+            timestamp: new Date().getTime()
+        },
+        {
+            id: 2,
+            label: "Lunes NEWC",
+            cryptocurrency: "Lunes",
+            address: "598crejoiewiojfepfepmqwp932iuj",
+            timestamp: new Date().getTime()
+        },
+        {
+            id: 3,
+            label: "ETH Chrome",
+            cryptocurrency: "Ethereum",
+            address: "598jrejoiewiojfepfvpmqwp932iuj",
+            timestamp: new Date().getTime()
+        },
+        {
+            id: 4,
+            label: "Freebitcoin",
+            cryptocurrency: "Bitcoin",
+            address: "698jrejoiewiojfepfepmqwp932ixj",
+            timestamp: new Date().getTime()
+        },
+    ],
+    2: [
+        {
+            id: 5,
+            label: "Lunes Wallet",
+            cryptocurrency: "Lunes",
+            address: "598jrejoiewiojfepfepmqwp932iuj",
+            timestamp: new Date().getTime()
+        },
+        {
+            id: 6,
+            label: "Lunes NEWC",
+            cryptocurrency: "Lunes",
+            address: "598crejoiewiojfepfepmqwp932iuj",
+            timestamp: new Date().getTime()
+        },
+        {
+            id: 7,
+            label: "ETH Chrome",
+            cryptocurrency: "Ethereum",
+            address: "598jrejoiewiojfepfvpmqwp932iuj",
+            timestamp: new Date().getTime()
+        },
+        {
+            id: 8,
+            label: "Freebitcoin",
+            cryptocurrency: "Bitcoin",
+            address: "698jrejoiewiojfepfepmqwp932ixj",
+            timestamp: new Date().getTime()
+        },
+    ],
+    3: [
+        {
+            id: 9,
+            label: "ETH Chrome",
+            cryptocurrency: "Ethereum",
+            address: "598jrejoiewiojfepfvpmqwp932iuj",
+            timestamp: new Date().getTime()
+        },
+        {
+            id: 10,
+            label: "Freebitcoin",
+            cryptocurrency: "Bitcoin",
+            address: "698jrejoiewiojfepfepmqwp932ixj",
+            timestamp: new Date().getTime()
+        },
+        {
+            id: 11,
+            label: "ETH Chrome",
+            cryptocurrency: "Ethereum",
+            address: "598jrejoiewiojfepfvpmqwp932iuj",
+            timestamp: new Date().getTime()
+        },
+        {
+            id: 12,
+            label: "Freebitcoin",
+            cryptocurrency: "Bitcoin",
+            address: "698jrejoiewiojfepfepmqwp932ixj",
+            timestamp: new Date().getTime()
+        },
+    ],
+    4: [
+        {
+            id: 13,
+            label: "ETH Chrome",
+            cryptocurrency: "Ethereum",
+            address: "598jrejoiewiojfepfvpmqwp932iuj",
+            timestamp: new Date().getTime()
+        },
+    ]
+};
 
 window.onload = () => {
-    injectWallets(wallets);
+    const pageViewed = localStorage.getItem('pageViewed');
+
+    if(!pageViewed){
+        localStorage.setItem('pageViewed', 1);
+    }
+
+    injectWallets(wallets[localStorage.getItem('pageViewed')]);
+    injectPagination(wallets);
 };
